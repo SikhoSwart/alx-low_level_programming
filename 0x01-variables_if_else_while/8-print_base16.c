1576ea51be361efff4661440028f2fd07d8b8921
SikhoSwart <sikhoswart@gmail.com>
2023-04-21 14:39:50 -0700

    C - variable, if, else, while

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+	int alph;
+
+	for (num = 48; num <= 57; num++)
+	{
+		putchar(num);
+	}
+	for (alph = 97; alph <= 102; alph++)
+	{
+		putchar(alph);
+	}
+	putchar('\n');
+	return (0);
+}
