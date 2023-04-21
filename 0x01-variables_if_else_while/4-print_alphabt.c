99fc1c4170514deee6fe4bfe260da65805a72498
SikhoSwart <sikhoswart@gmail.com>
2023-04-21 13:47:02 -0700

    C - variable, if, else, while

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	char alph;
+
+	for (alph = 97; alph <= 122; alph++)
+	{
+		if (alph == 101 || alph == 113)
+		{
+			alph++;
+			continue;
+		}
+		putchar(alph);
+
+	}
+	putchar('\n');
+	return (0);
+}
