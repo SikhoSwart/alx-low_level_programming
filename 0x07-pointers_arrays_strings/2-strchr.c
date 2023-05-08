d22a8cab1102c857a95c92f89934f88e45062f9a
SikhoSwart <sikhoswart@gmail.com>
2023-05-08 08:41:20 -0700

    pointers_arrays_strings

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,20 +11,12 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i] >= '\0'; i++)
 	{
-		if (c == '\0')
-		{
-			return (s);
-		}
-		if (s[i] == '\0')
-		{
-			return (s);
-		}
 		if (s[i] == c)
 		{
 			return (&s[i]);
 		}
 	}
-	return ('\0');
+	return (0);
 }
