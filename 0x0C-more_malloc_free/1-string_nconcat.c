f1dd3b7ac90f1f9ab40cfbd664a033ee64906316
SikhoSwart <sikhoswart@gmail.com>
2023-05-18 07:34:10 -0700

    Malloc and free

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -9,7 +9,7 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i = 0, j = 0;
+	unsigned int i = 0, j = 0, k, l = 0;
 	char *conc_str;
 
 	while (s1[i] != '\0')
@@ -22,33 +22,29 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	if (n >= j)
 	{
-		conc_str = malloc(sizeof(char) * (i + j + 1));
+		n = j;
 	}
-	else if (n < j)
-	{
-		conc_str = malloc(sizeof(char) * (i + n + 1));
-	}
-	if (conc_str == NULL)
+	conc_str = malloc(sizeof(char) * (i + n + 1));
+	if (!conc_str)
 	{
 		return (NULL);
 	}
 	if (s1 != NULL)
 	{
-		for (i = 0; s1[i] != '\0'; i++)
+		for (k = 0; s1[k] != '\0'; k++)
 		{
-			conc_str[i] = s1[i];
+			conc_str[k] = s1[k];
 		}
 	}
 	if (s2 != NULL)
 	{
-		j = 0;
-		while (s2[j] != '\0' && j < n)
+		while (s2[l] && l < n)
 		{
-			conc_str[i] = s2[j];
-			i++;
-			j++;
+			conc_str[k] = s2[l];
+			l++;
+			k++;
 		}
 	}
-	conc_str[i] = '\0';
+	conc_str[k] = '\0';
 	return (conc_str);
 }
